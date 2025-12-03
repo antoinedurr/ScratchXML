74506a740fb28eada043f4ae48be26ed99061dcb
Antoine Durr <antoine@antoinedurr.com>
2025-12-03 00:03:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8") if (HERE / "
 
 setup(
     name="scratchxml",
-    version="0.1.5",
+    version="0.1.6",
     description="Read and write Assimilate Scratch custom-command XML as Python objects",
     long_description=long_description,
     long_description_content_type="text/markdown",
