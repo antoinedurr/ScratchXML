fba18751b1d0e08fa5220ff1e45739bbac5f9d8e
Antoine Durr <antoine@antoinedurr.com>
2025-12-11 08:55:19 -0800

    create example3

---
+++ getting_started/scratch_example3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# https://github.com/antoinedurr/ScratchXML/blob/main/getting_started/scratch_example3.py
+# Reverse the order of shots in each slot
+#
+from scratchXML import Scratch
+
+# read cmd-0.xml and convert into a Scratch() hierarchy
+scratch = Scratch(xml='xml/example3.xml')
+
+print([shot.name for shot in scratch.constructs[0].slots[0].shots])  # before reversal
+
+for construct in scratch.constructs:
+  for slot in construct.slots:
+    slot.shots = reversed(slot.shots)  # see limitations
+
+print([shot.name for shot in scratch.constructs[0].slots[0].shots]) # after reversal
+
+scratch['@update'] = "Y"               # tell Scratch to update from the resulting xml
+scratch.write('xml/output.xml')        # write out resulting file
