68245718e8aa87aaa95142c377664ee13d2608d8
Antoine Durr <antoine@antoinedurr.com>
2025-12-15 10:46:15 -0800

    added example4

---
+++ getting_started/scratch_example4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# https://github.com/antoinedurr/ScratchXML/blob/main/getting_started/scratch_example4.py
+# Reverse the order of shots in each slot
+#
+from scratchXML import Scratch
+from scratchXML import scratchparse  # Argparse subclass specific to ScratchXML 
+import pprint
+
+parser = scratchparse(usage="Print out info for each shot, including metadata dict", wait_til_finished=False)
+args = parser.parse_args()
+
+# ‘inputxml’ is the standard args attribute for the XML that Scratch writes out to Temp
+scratch = Scratch(xml=args.inputxml)
+timeline = scratch.constructs[0]
+
+shots = timeline.shots(selected=True) or timeline.shots()
+for shot in shots:
+   # print out metadata for each shot
+   print(f"Shot: {shot.name} Slot: {shot.slot} Layer: {shot.layer} Metadata:\n{pprint.pformat(shot.metadata, indent=4)}")
